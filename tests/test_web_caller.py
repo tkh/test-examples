1004cc88032e816116bd46f2eb66e4b89f3f766f
Tyler Hildebrandt <contact@tylerhildebrandt.com>
2016-12-23 11:03:16 +0000

    Add test examples to assert against exceptions

--- tests/test_web_caller.py
+++ tests/test_web_caller.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
+from requests.exceptions import ConnectionError
 from mock import NonCallableMock, patch
 
 from modules.web_caller import get_google, GOOGLE_URL
+
+MOCK_GOOGLE_URL = 'http://not-going-to-work!!!'
 
 
 class TestWebCaller(TestCase):
@@ -33,3 +36,31 @@ class TestWebCaller(TestCase):
 
         # Check that the mocked response.status_code is as expected
         self.assertEqual(200, response.status_code)
+
+    @patch('modules.web_caller.GOOGLE_URL', MOCK_GOOGLE_URL)
+    def test_get_google_with_exception(self):
+        """
+        Call the `get_google` function while using `patch` to create an
+        unreachable URL.
+
+        Assert that the error happened and capture it using assertRaises.
+        """
+
+        # Call the function via assertRaises and confirm the exception is
+        # raised by making the call
+        self.assertRaises(ConnectionError, get_google)
+
+    @patch('modules.web_caller.GOOGLE_URL', MOCK_GOOGLE_URL)
+    def test_get_google_with_exception_context_manager(self):
+        """
+        Call the `get_google` function while using `patch` to create an
+        unreachable URL.
+
+        Assert that the error happened and capture it using the assertRaises
+        context manager.
+        """
+
+        # Establish an assertRaises context manager
+        with self.assertRaises(ConnectionError):
+            # Call the function
+            get_google()
