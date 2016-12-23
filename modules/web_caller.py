be7ea12d2b95da9f58c19bf51c267892738c6bb0
Tyler Hildebrandt <contact@tylerhildebrandt.com>
2016-12-23 11:43:32 +0000

    Simplify the get_google function

--- modules/web_caller.py
+++ modules/web_caller.py
@@ -5,6 +5,4 @@ GOOGLE_URL = 'http://www.google.com'
 
 
 def get_google():
-    response = requests.get(GOOGLE_URL)
-    response.raise_for_status()
-    return response
+    return requests.get(GOOGLE_URL)
